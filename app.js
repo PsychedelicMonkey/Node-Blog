cfd26881f90c735362b9c99cdd0abbd20be68658
PsychedelicMonkey <ryan.getz.96@gmail.com>
2021-08-19 10:22:00 -0600

    Project setup

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const path = require('path');
+const morgan = require('morgan');
+const expressLayout = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(expressLayout);
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('MongoDB Connected....'))
+  .catch((err) => console.log(err));
+
+// Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
